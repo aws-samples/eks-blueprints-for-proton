ce5070c15f3fa3772851beefa9205818683b1d4b
Massimo Re Ferre <massimo@it20.info>
2022-03-02 14:27:32 +0100

    Fixed the outputs.tf file syntax

--- templates/eks-mng-karpenter-with-new-vpc/v1/infrastructure/outputs.tf
+++ templates/eks-mng-karpenter-with-new-vpc/v1/infrastructure/outputs.tf
@@ -1,4 +1,4 @@
 output "configure_kubectl" {
   description = "Outputs of Blueprints module"
-  value       = "${module.aws-eks-accelerator-for-terraform.configure_kubectl}"
+  value       = module.aws-eks-accelerator-for-terraform.configure_kubectl
 }
