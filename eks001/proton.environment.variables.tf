2e97b18667cef16255c7ff2bb4d435b96f922575
sbx_user1051 <sbx_user1051@169.254.18.177>
2022-03-02 10:00:40 +0000

    Update requested for Proton - environment/eks001

---
+++ eks001/proton.environment.variables.tf
@@ -0,0 +1,16 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-west-2:693935722839:environment/eks001
+
+If the resource is no longer is accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+variable "environment" {
+  type = object({
+    inputs = map(string)
+    name   = string
+  })
+  default = null
+}
+
