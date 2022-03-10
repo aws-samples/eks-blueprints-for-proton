2f377ffe36d10cb842e31cad5512178a5393d059
Apoorva Kulkarni <kuapoorv@amazon.com>
2022-03-10 16:43:45 -0500

    add aws_caller_identity data source

--- templates/eks-mng-karpenter-with-new-vpc/v1/infrastructure/data.tf
+++ templates/eks-mng-karpenter-with-new-vpc/v1/infrastructure/data.tf
@@ -4,6 +4,8 @@ data "aws_availability_zones" "available" {}
 
 data "aws_partition" "current" {}
 
+data "aws_caller_identity" "current" {}
+
 data "aws_eks_cluster" "cluster" {
   name = module.aws-eks-accelerator-for-terraform.eks_cluster_id
 }
