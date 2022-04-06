f6ae6278013d1c13a63f65f39e0f9a32b1e66d16
Kevin Coleman <kcaws@amazon.com>
2022-04-05 20:30:33 -0700

    Adding output for admin access

--- templates/eks-mng-karpenter-with-new-vpc/v1/infrastructure/outputs.tf
+++ templates/eks-mng-karpenter-with-new-vpc/v1/infrastructure/outputs.tf
@@ -1,4 +1,10 @@
 output "configure_kubectl" {
-  description = "Outputs of Blueprints module"
+  description = "Command to configur admin access to the EKS cluster for identity that created the cluter."
   value       = module.aws-eks-accelerator-for-terraform.configure_kubectl
+}
+
+output "configure_admin_kubectl" {
+  description = "Command to configure admin access the EKS cluster."
+  value       = module.aws-eks-accelerator-for-terraform.teams[0].platform_teams_configure_kubectl["platform-team"]
+
 }
