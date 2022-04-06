25fd9eb9d77cd290479d8c989c418f1f1eb85e07
Massimo Re Ferrè <massimo@it20.info>
2022-04-06 18:09:24 +0200

    Updated outputs.tf to include the full command

--- templates/eks-mng-karpenter-with-new-vpc/v1/infrastructure/outputs.tf
+++ templates/eks-mng-karpenter-with-new-vpc/v1/infrastructure/outputs.tf
@@ -1,4 +1,6 @@
-output "configure_kubectl" {
-  description = "Outputs of Blueprints module"
-  value       = module.aws-eks-accelerator-for-terraform.configure_kubectl
+output "platform_teams_configure_kubectl" {
+  description = "Configure kubectl for each Platform Team."
+  value = tomap({
+    for k, v in module.aws-eks-accelerator-for-terraform.teams[0].platform_teams_iam_role_arn : k => "aws eks --region ${data.aws_region.current.id} update-kubeconfig --name ${data.aws_eks_cluster.cluster.name}  --role-arn ${v}"
+  })["platform-team"]
 }
