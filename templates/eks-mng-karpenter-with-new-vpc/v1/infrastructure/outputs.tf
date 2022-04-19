8fca6b97f2d9d58931b8e7bb5c7d3e3952d83166
Massimo Re Ferre <massimo@it20.info>
2022-04-19 15:54:23 +0200

    Rmoved 2 outputs

--- templates/eks-mng-karpenter-with-new-vpc/v1/infrastructure/outputs.tf
+++ templates/eks-mng-karpenter-with-new-vpc/v1/infrastructure/outputs.tf
@@ -4,11 +4,3 @@ output "platform_teams_configure_kubectl" {
     for k, v in module.eks_blueprints.teams[0].platform_teams_iam_role_arn : k => "aws eks --region ${data.aws_region.current.id} update-kubeconfig --name ${data.aws_eks_cluster.cluster.name}  --role-arn ${v}"
   })["platform-team"]
 }
-
-output "eks_cluster_id" {
-  value = module.eks_blueprints.eks_cluster_id
-}
-
-output "eks_cluster_name" {
-  value = module.eks_blueprints.eks_cluster_name
-}
