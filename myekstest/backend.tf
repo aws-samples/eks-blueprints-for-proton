8d6f7414ebe91bd772b080222110cb8fce58b715
Massimo Re Ferre <massimo@it20.info>
2022-04-21 11:22:24 +0200

    Cleaned PR folders + reset of env_config.json file

--- myekstest/backend.tf
+++
@@ -1,7 +0,0 @@
-/*
-This file is no longer managed by AWS Proton. The associated resource has been deleted in Proton.
-*/
-
-terraform {
-  backend "s3" {}
-}
--- myekstest/variables.tf
+++
@@ -1,8 +0,0 @@
-/*
-This file is no longer managed by AWS Proton. The associated resource has been deleted in Proton.
-*/
-
-variable "aws_region" {
-  type    = string
-  default = "us-west-2"
-}
