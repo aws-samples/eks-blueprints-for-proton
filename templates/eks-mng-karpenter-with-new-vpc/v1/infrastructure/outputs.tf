d57270ca1a5c18a8f93b981091b3917af5813c3e
Massimo Re Ferre <massimo@it20.info>
2022-04-26 12:29:24 +0200

    Updated outputs to include cluster version and ONE addon

--- templates/eks-mng-karpenter-with-new-vpc/v1/infrastructure/outputs.tf
+++ templates/eks-mng-karpenter-with-new-vpc/v1/infrastructure/outputs.tf
@@ -1,10 +1,21 @@
 output "platform_teams_configure_kubectl" {
-  description = "Configure kubectl for each Platform Team."
+  description = "The command to use to configure the kubeconfig file to be used with kubectl."
   value = tomap({
     for k, v in module.eks_blueprints.teams[0].platform_teams_iam_role_arn : k => "aws eks --region ${data.aws_region.current.id} update-kubeconfig --name ${data.aws_eks_cluster.cluster.name}  --role-arn ${v}"
   })["platform-team"]
 }
 
 output "eks_cluster_id" {
+  description = "The name of the EKS cluster."
   value = module.eks_blueprints.eks_cluster_id
+}
+
+output "eks_cluster_version" {
+  description = "The version of the EKS cluster."
+  value = local.eks_cluster_version
+}
+
+output "aws_load_balancer_controller" {
+  description = "AWS Load Balancer controller."
+  value = var.environment.inputs.aws_load_balancer_controller
 }
