3e9117183a7021570c627c71a96faa76ff565b05
Massimo Re Ferre <massimo@it20.info>
2022-05-03 09:47:59 +0200

    Tweaked Outputs.tf + env_conf for tests

--- templates/eks-mng-karpenter-with-new-vpc/v1/infrastructure/outputs.tf
+++ templates/eks-mng-karpenter-with-new-vpc/v1/infrastructure/outputs.tf
@@ -10,3 +10,12 @@ output "eks_cluster_id" {
   value       = module.eks_blueprints.eks_cluster_id
 }
 
+output "eks_cluster_version" {
+  description = "The version of the EKS cluster."
+  value       = module.eks_blueprints.cluster_version
+}
+
+output "enable_aws_load_balancer_controller" {
+  description = "The flag for the Load Balancer controller."
+  value       = module.eks_blueprints.enable_aws_load_balancer_controller
+}
