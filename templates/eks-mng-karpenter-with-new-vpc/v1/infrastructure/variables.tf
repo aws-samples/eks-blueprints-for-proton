73d81e9aec6f955903dd49a8fd695d4c325c3d8d
Kevin Coleman <kcaws@amazon.com>
2022-05-06 08:36:20 -0700

    Adding Karpenter back

--- templates/eks-mng-karpenter-with-new-vpc/v1/infrastructure/variables.tf
+++ templates/eks-mng-karpenter-with-new-vpc/v1/infrastructure/variables.tf
@@ -2,34 +2,3 @@ variable "aws_region" {
   type    = string
   default = "us-west-1"
 }
-
-variable "environment_for_local_test" {
-  type = object({
-    inputs = object({
-      kubernetes_version           = any
-      cluster_name                 = any
-      vpc_cidr                     = any
-      user                         = any
-      aws_load_balancer_controller = any
-      metrics_server               = any
-      aws_for_fluentbit            = any
-      cert_manager                 = any
-      vpa                          = any
-      karpenter                    = any
-    })
-  })
-  default = {
-    inputs = {
-      kubernetes_version           = "1.21"
-      cluster_name                 = "proton-blueprint-cluster"
-      vpc_cidr                     = "10.0.0.0/16"
-      user                         = "<user>"
-      aws_load_balancer_controller = true
-      metrics_server               = true
-      aws_for_fluentbit            = true
-      cert_manager                 = true
-      vpa                          = true
-      karpenter                    = true
-    }
-  }
-}
