52c6958e42512dd11e8ba548845705abc93e3f54
Massimo Re Ferrè <massimo@it20.info>
2022-05-06 14:01:54 +0200

    Minor fixes (#36)

--- templates/eks-mng-karpenter-with-new-vpc/v1/infrastructure/outputs.tf
+++ templates/eks-mng-karpenter-with-new-vpc/v1/infrastructure/outputs.tf
@@ -19,3 +19,28 @@ output "enable_aws_load_balancer_controller" {
   description = "The flag for the Load Balancer controller."
   value       = var.environment.inputs.aws_load_balancer_controller
 }
+
+output "enable_karpenter" {
+  description = "The flag for Karpenter."
+  value       = var.environment.inputs.karpenter
+}
+
+output "enable_metrics_server" {
+  description = "The flag for the Metric Server."
+  value       = var.environment.inputs.metrics_server
+}
+
+output "enable_aws_for_fluentbit" {
+  description = "The flag for the Fluentbit."
+  value       = var.environment.inputs.aws_for_fluentbit
+}
+
+output "enable_cert_manager" {
+  description = "The flag for Certificate Manager."
+  value       = var.environment.inputs.cert_manager
+}
+
+output "enable_vpa" {
+  description = "The flag for Virtual Pod Autoscaler."
+  value       = var.environment.inputs.vpa
+}
